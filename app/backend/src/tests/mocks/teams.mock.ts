0dfb3c54d5f734b017fe3b73822652590b837aec
lucasalmeida30 <lucas_almeida12@icloud.com>
2023-05-15 16:56:59 -0300

    erros nos testes

--- app/backend/src/tests/mocks/teams.mock.ts
+++ app/backend/src/tests/mocks/teams.mock.ts
@@ -13,4 +13,4 @@ const teamsMock = [
     },
   ]
 
-  export default teamsMock;
+  export { teamsMock }
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -4,12 +4,8 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 import { app } from '../app';
 import TeamModel from '../database/models/team'
-import teamsMock from './mocks/teams.mock';
+import { teamsMock } from './mocks/teams.mock';
 
-
-import { Response } from 'superagent';
-import { before, after } from 'node:test';
-import { response } from 'express';
 
 chai.use(chaiHttp);
 
@@ -19,41 +15,40 @@ describe('testando a rota de teams', () => {
   /**
    * Exemplo do uso de stubs com tipos
    */
-  let chaiHttpResponse: Response;
   afterEach(()=>{
-    sinon.restore();
+    sinon.restore()
   })
 
   it('se consegue retornar todos os times com o método GET', async () => {
     sinon.stub(TeamModel, 'findAll').resolves(teamsMock as TeamModel[])
-    chaiHttpResponse = await chai
+    const response = await chai
        .request(app)
        .get('/teams')
       
-    expect(chaiHttpResponse.status).to.be.equal(200)
-    expect(chaiHttpResponse.body).to.be.deep.equal(teamsMock)
+    expect(response.status).to.be.equal(200)
+    expect(response.body).to.be.deep.equal(teamsMock)
 
   });
 
   it('deve retornar undefined com id invalido', async () => {
     sinon.stub(TeamModel, 'findByPk').resolves(undefined)
-    chaiHttpResponse = await chai
+    const response = await chai
        .request(app)
        .get('/teams/697')
       
-    expect(chaiHttpResponse.status).to.be.equal(404)
-    expect(chaiHttpResponse.body).to.be.deep.equal('team nao encontrado')
+    expect(response.status).to.be.equal(404)
+    expect(response.body).to.be.deep.equal('team nao encontrado')
 
   });
 
   it('deve retornar status 200 e elemento certo', async () => {
     sinon.stub(TeamModel, 'findByPk').resolves(teamsMock[1] as TeamModel)
-    chaiHttpResponse = await chai
+    const response = await chai
        .request(app)
        .get('/teams/1')
       
-    expect(chaiHttpResponse.status).to.be.equal(200)
-    expect(chaiHttpResponse.body).to.be.deep.equal(teamsMock[1])
+    expect(response.status).to.be.equal(200)
+    expect(response.body).to.be.deep.equal(teamsMock[1])
 
   });
 
