3c205e9d5a4c263c82b467f3f343b6b94d6fd9a0
lucasalmeida30 <lucas_almeida12@icloud.com>
2023-05-18 19:54:25 -0300

    adiciona testes na rota login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,83 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from '../app';
+import * as jsonwebtoken from 'jsonwebtoken';
+import user from './mocks/user.mock';
+import User from '../database/models/user';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('testando a rota login', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+  afterEach(()=>{
+    sinon.restore()
+  })
+
+  it('se a API retorna um status 200 com os dados correto', async () => {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsInJvbGUiOiJhZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicGFzc3dvcmQiOiIkMmEkMDgkeGkuSHhrMWN6QU8wblpSLi5CMzkzdTEwYUVEMFJRMU4zUEFFWFE3SHh0TGpLUEVaQnUuUFciLCJpYXQiOjE2ODQ0NDg5NzEsImV4cCI6MTY4NDYyMTc3MX0.D5B4isLanJjbUXpyCkrO2tpmPa8rDXpdnPcW1DoBlTY'
+
+    sinon.stub(User, "findOne").resolves(user as unknown as User);
+    sinon.stub(jsonwebtoken, 'sign').resolves(token);
+    const response = await chai
+       .request(app)
+       .post('/login')
+       .send({ email: 'user@user.com', password: 'secret_user'})
+
+      
+    expect(response.status).to.be.equal(200)
+    expect(response.body).to.be.deep.equal({ token })
+
+  });
+
+  it('sem o email deve retornar um status 400 ', async () => {
+    const response = await chai
+       .request(app)
+       .post('/login')
+       .send({ password: 'secret_user'})
+      
+    expect(response.status).to.be.equal(400)
+    expect(response.body).to.be.deep.equal('All fields must be filled')
+
+  });
+
+  it('sem a password deve retornar status 400', async () => {
+    const response = await chai
+       .request(app)
+       .get('/login')
+       .send({ email: 'user@user.com'})
+      
+    expect(response.status).to.be.equal(400)
+    expect(response.body).to.be.deep.equal('All fields must be filled')
+
+  });
+
+  it('se nao passar uma senha valida retorna erro', async () => {
+    const response = await chai
+       .request(app)
+       .get('/login')
+       .send({ email: 'user@user.com', password: 'senhasenha'})
+      
+    expect(response.status).to.be.equal(401)
+    expect(response.body).to.be.deep.equal('Invalid email or password')
+
+  });
+
+  it('se nao passar uma senha valida retorna erro', async () => {
+    const response = await chai
+       .request(app)
+       .get('/login')
+       .send({ email: 'namee@user.com', password: 'secret_user'})
+      
+    expect(response.status).to.be.equal(401)
+    expect(response.body).to.be.deep.equal('Invalid email or password')
+
+  });
+ 
+});
